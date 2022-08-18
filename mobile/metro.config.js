0f3932fd8a148d9b7c0ffa516c3c13be2aa0e304
vincentmegia <vincent.megia@gmail.com>
2022-08-18 22:07:00 +0800

    feat: fixed mono repo metro bundler issues

---
+++ mobile/metro.config.js
@@ -0,0 +1,32 @@
+const path = require("path")
+const exclusionList = require("metro-config/src/defaults/exclusionList")
+
+// const moduleRoot = path.resolve(__dirname)
+const moduleRoot = path.resolve(__dirname, "..")
+const packages = `${moduleRoot}/packages`
+console.log("--------------------moduleRoot path: ", moduleRoot)
+console.log("--------------------packages path: ", packages)
+
+module.exports = {
+   projectRoot: moduleRoot,
+   // We need to make sure that only one version is loaded for peerDependencies
+   // So we block them at the root, and alias them to the versions in example's node_modules
+   resolver: {
+      extraNodeModules: {
+         react: `${moduleRoot}/node_modules/react`,
+         "react-native": `${moduleRoot}/node_modules/react`,
+      },
+      blockList: exclusionList([
+         new RegExp(`${packages}/stupendousware-core/node_modules/react/.*`),
+         new RegExp(`${packages}/stupendousware-core/example/.*`),
+      ]),
+   },
+   transformer: {
+      getTransformOptions: async () => ({
+         transform: {
+            experimentalImportSupport: false,
+            inlineRequires: true,
+         },
+      }),
+   },
+}
---
+++ mobile/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { SafeAreaView, Text } from "react-native"
+import { Splash } from "@stupendousware/core"
+
+const App = () => {
+   return (
+      <SafeAreaView>
+         <Text>asdsdfsdf</Text>
+         <Splash />
+      </SafeAreaView>
+   )
+}
+
+export default App
